599571e2ab4bfdea713572375f5499f032d25032
Amoda566 <91647261+Amoda566@users.noreply.github.com>
2024-04-12 00:35:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  TOKEN: "MTIyNzM0OTMxNDMwMTU5MTU4Mg.Gd_co-.QhEiGCtgiQSuuCOvNsCkO98BaIfn1r1cH84pbw",
+  ownerID: ["848553423170830366", ""],
+  botInvite: "https://discord.com/oauth2/authorize?client_id=1226954434408615998&permissions=8&scope=bot",
+  supportServer: "https://discord.gg/EVVg7pDN",
+  mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  status: 'RTXX GG',
+  commandsDir: './commands',
+  language: "en",
+  embedColor: "00fbff",
+  errorLog: "",
+
+
+  sponsor: {
+    status: true,
+    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+  },
+
+  voteManager: {
+    status: false,
+    api_key: "",
+    vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
+    vote_url: "",
+  },
+
+  shardManager: {
+    shardStatus: false
+  },
+
+  playlistSettings: {
+    maxPlaylist: 10,
+    maxMusic: 75,
+  },
+
+  opt: {
+    DJ: {
+      commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume', 'shuffle']
+    },
+
+    voiceConfig: {
+      leaveOnFinish: false,
+      leaveOnStop: false,
+      leaveOnEmpty: {
+        status: true,
+        cooldown: 10000000,
+      },
+
+    },
+
+    maxVol: 150,
+
+  }
+}
